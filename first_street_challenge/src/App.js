a4f369f775823a9dc89dff535bd2b3635eb68915
Daniel Park <dpeace926@gmail.com>
2018-04-04 22:40:33 -0400

    create stylesheets for header, main, footer

--- first_street_challenge/src/App.js
+++ first_street_challenge/src/App.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Components
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+// CSS
+import './styles/Header.css'
+import './styles/Main.css'
+import './styles/Footer.css'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Main />
+        <Footer />
       </div>
     );
   }
