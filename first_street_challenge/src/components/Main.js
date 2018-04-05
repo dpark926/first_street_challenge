9c9cbc18259189f3233f797d6ca23435e858b5f2
Daniel Park <dpeace926@gmail.com>
2018-04-05 17:47:19 -0400

    eject and implement css modules

--- first_street_challenge/src/components/Main.js
+++ first_street_challenge/src/components/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import mainStyles from '../styles/Main.css'
 
 function Main () {
   return (
     <main>
       <h1>Main</h1>
+      <div className={ mainStyles.test }>test</div>
     </main>
   )
 }
