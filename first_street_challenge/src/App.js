a0fc7ddf473060a0ee6169a2471077d42751eb53
Daniel Park <dpeace926@gmail.com>
2018-04-05 20:18:57 -0400

    install and configur react_hot_loader

--- first_street_challenge/src/App.js
+++ first_street_challenge/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
 // import './App.scss';
 
 // Components
@@ -21,4 +22,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default hot(module)(App);
