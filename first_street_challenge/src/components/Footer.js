753f58d1d85f91ae70e274d5dc458acc203badba
Daniel Park <dpeace926@gmail.com>
2018-04-06 17:54:37 -0400

    build comps for footer

--- first_street_challenge/src/components/Footer.js
+++ first_street_challenge/src/components/Footer.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import style from '../styles/Footer.scss'
+import style from '../styles/Footer.scss';
+import logo from '../assets/logo.png';
 
 function Footer () {
   return (
-    <footer>
-      <h1>Footer</h1>
-      <div className={ style.test }>test</div>
+    <footer className={ style.footerContainer }>
+      <div className={ style.footerTop }>
+        <div className={ style.footerTopLinks }>
+          <h5 className={ style.footerTopHeader }>Fakesite</h5>
+          <ul>
+            <li>About us</li>
+            <li>Press</li>
+            <li>Policies</li>
+            <li>Help</li>
+          </ul>
+        </div>
+        <div className={ style.footerTopLinks }>
+          <h5 className={ style.footerTopHeader }>Account</h5>
+          <ul>
+            <li>Edit Profile</li>
+            <li>Friends</li>
+            <li>Social</li>
+            <li>Delete Profile</li>
+          </ul>
+        </div>
+      </div>
+      <div className={ style.footerBottom }>
+        <div className={ style.logoContainer }>
+          <a href='#'>
+            <img src={ logo }></img>
+          </a>
+        </div>
+        <div className={ style.linkContainer }>
+          <ul>
+            <li>Terms</li>
+            <li>Privacy</li>
+            <li>Site Map</li>
+          </ul>
+        </div>
+      </div>
     </footer>
   )
 }
