8a06ea46e1b6bd842efd69d98725aa43e0468af3
Daniel Park <dpeace926@gmail.com>
2018-04-08 21:48:53 -0400

    play button overlay wip

--- first_street_challenge/src/components/Main.js
+++ first_street_challenge/src/components/Main.js
@@ -5,15 +5,19 @@ import stockImg from '../assets/stockimage.png';
 function Main () {
   return (
     <main className={ style.mainContainer }>
-      <div className={ style.mainTop }>
+      <article className={ style.mainTop }>
         <div className={ style.mainTopLeft }>
           <h2 className={ style.mainTopHeader }>Video Title (from videos.json)</h2>
           <p className={ style.mainTopPara }>Video Description (from videos.json)</p>
         </div>
-        <div className={ style.mainTopRight }>
-          <img src={ stockImg }></img>
-        </div>
-      </div>
+        <figure className={ style.mainTopRight }>
+          <a href='#'>
+            <div>
+              <img src={ stockImg }></img>
+            </div>
+          </a>
+        </figure>
+      </article>
       <div className={ style.mainBottom }>
         <h2>Ready to have your cake and eat it too?</h2>
         <p>Start by designing the experience you have in mind. We'll guide you through each step. If your experience meets the quality standards, you'll hear more about what's next.</p>
